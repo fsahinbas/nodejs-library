155eea5d8c678fa3e5ad3f3cc8d5f692fe795ae4
fsahinbas <fsahinbas@gmail.com>
2025-06-11 00:55:21 +0300

    initializing project

---
+++ server.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
+import indexRouter from "./routes/index.js";
+import mongoose from "mongoose";
+import path from "path";
+const __dirname = path.resolve();
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressEjsLayouts);
+app.use("/", indexRouter);
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+app.listen(process.env.PORT || 3000);
